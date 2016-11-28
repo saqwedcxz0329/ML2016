85723b844528d916d3eb97dd64afecdfd0f662ac
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2016-11-28 18:17:17 +0800

    beyond baseline(tf-idf+lsa)

--- hw4/cluster.py
+++ hw4/cluster.py
@@ -8,10 +8,11 @@ from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
 from gensim.models import doc2vec
-from nltk.corpus import stopwords
 from stop_words import get_stop_words
+import logging
 
 cluster_num = 20
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 def parseData():
     check_index = open('check_index.csv', 'r')
@@ -23,8 +24,17 @@ def parseData():
         row_data['x_ID'] = int(line[1])
         row_data['y_ID'] = int(line[2].split('\n')[0])
         check_list.append(row_data)
+    check_index.close()
     del check_index
     return check_list
+
+def parseTitle():
+    file = open('process_title.txt', 'r')
+    title_list = []
+    for line in file:
+        title_list.append(line.split())
+    file.close()
+    return title_list
 
 def predict(predict_title, check_list):
     predict_file = open("predict.csv", "w")
@@ -35,27 +45,37 @@ def predict(predict_title, check_list):
         y_index = row['y_ID']
         ans = int(predict_title[x_index] == predict_title[y_index])
         predict_file.write("%d,%d\n" %(i, ans))
+    predict_file.close()
 
-def testGensim(doc):
-	file = open("test.txt", "w+")
-	stopwords = set(get_stop_words('english'))
-	stopwords.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', ' '])
-	text = 'Hello bye the the. hi . !'
-	pre_sentences = []
-	for line in doc:
-		if line != '\n':
-			text = ' '.join([word for word in line.lower().split() if word not in stopwords])
-			pre_sentences.append(text)
-			if text != '':
-				file.write(text)
-				file.write('\n')
-	sentences=doc2vec.TaggedLineDocument(file)
-	model = doc2vec.Doc2Vec(sentences,size = 100, window = 300, min_count = 10, workers=4)
-	for line in sentences:
-		print line
+def preProcessingDoc(doc, output_file_name):
+    file = open(output_file_name, "w+")
+    stopwords = set(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')',
+         '[', ']', '{', '}', ' ', '==', '!=', '>', '<', '//', '};',
+          '=', '--', '||', '&&', '+', '-', '*', '/', '_', '&', '#'])
+    """
+    stopwords = set(get_stop_words('english'))
+    stopwords.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')',
+         '[', ']', '{', '}', ' ', '==', '!=', '>', '<', '//', '};',
+          '=', '--', '||', '&&', '+', '-', '*', '/', '_', '&', '#'])
+    """
+    for line in doc:
+        if line != '\n':
+            text = ' '.join([word for word in line.lower().split() if word not in stopwords])
+            if text != '':
+                file.write(text)
+                file.write('\n')
+    file.close()
+
+def wordVector(doc, title):
+    sentences=doc2vec.TaggedLineDocument(doc)
+    model = doc2vec.Doc2Vec(size = 100, window = 300, min_count = 5, workers=10, alpha=0.025, min_alpha = 0.025)
+    model.build_vocab(sentences)
+    model.train(sentences)
+
+    return model
 
 def dimensionReduction(X):
-    svd = TruncatedSVD(2)
+    svd = TruncatedSVD(15)
     normalizer = Normalizer(copy=False)
     lsa = make_pipeline(svd, normalizer)
     X = lsa.fit_transform(X)
@@ -68,33 +88,51 @@ def visulization(X):
         plt.plot(point[0], point[1], '.')
     plt.show()
 
-doc = open('docs.txt', "r")
-title = open('title_StackOverflow.txt', 'r')
-
-testGensim(doc)
+#preProcessingDoc(open('docs.txt', "r"), 'process_doc.txt')
+#doc = open('process_doc.txt', 'r')
+preProcessingDoc(open('title_StackOverflow.txt', 'r'), 'process_title.txt')
+title = open('process_title.txt', 'r')
 
 """
+model = wordVector(doc, title)
+title_list = parseTitle()
+for i in range(len(title_list)):
+    title = title_list[i]
+    title_list[i] = model.infer_vector(title)
+
+km = KMeans(n_clusters=cluster_num, init='k-means++', max_iter=500, n_init=1,
+                verbose=True)
+
+# Fit and predict the title
+predict_title = km.fit_predict(title_list)
 check_list = parseData()
+predict(predict_title, check_list)
+"""
+
+###################################################################
+
 vectorizer = TfidfVectorizer(max_df=0.5, max_features=None,
                                  min_df=2, stop_words='english')
 
-# Generate word vector
-vectorizer.fit(title)
+# Generate features vector
+#vectorizer.fit(doc)
 
-# Transform title to word vector
-X = vectorizer.transform(title)
+# Transform title to features vector
+#X = vectorizer.transform(title)
+
 X = vectorizer.fit_transform(title)
 
-#X = dimensionReduction(X)
+print X.shape
+
+X = dimensionReduction(X)
 
 km = KMeans(n_clusters=cluster_num, init='k-means++', max_iter=100, n_init=1,
                 verbose=True)
 
 # Fit and predict the title
 predict_title = km.fit_predict(X)
+check_list = parseData()
 predict(predict_title, check_list)
-
 
 # Visualization
 #visulization(X)
-"""
